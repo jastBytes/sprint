a08db1a2c000a00c88acc91e06806d9a158d3ed6
Jan Steffen <github@jast-bytes.de>
2024-11-22 09:36:03 +0100

    Create hash.go

---
+++ hash.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+)
+
+// Hash returns a SHA256 hash of the given struct
+func Hash[T any](s T) []byte {
+	var b bytes.Buffer
+	gob.NewEncoder(&b).Encode(s)
+	h := sha256.New()
+	h.Write(b.Bytes())
+	return h.Sum(nil)
+}
+
+// Hash returns a SHA256 hash of the given struct as string
+func HashString[T any](s T) string {
+	return fmt.Sprintf("%x\n", Hash(s))
+}
